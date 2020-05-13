7b5cb4ec76e2b071532f9e0557a643b0129accf2
Nelson198 <nteixeira88@gmail.com>
2020-05-14 00:59:41 +0100

    #44 Commit: TP2 - Top10 (batch)

--- TP2/TP2.md
+++ TP2/TP2.md
@@ -2,6 +2,64 @@
 
 ## TP2
 
+### *Batch*
+
+* *Hadoop* :
+
+    ```bash
+    $ git clone https://github.com/big-data-europe/docker-hadoop.git
+    $ cd docker-hadoop
+    $ docker-compose up
+    ```
+
+* *Spark* :
+
+    ```bash
+    $ git clone https://github.com/big-data-europe/docker-spark.git
+    $ cd docker-spark
+    $ docker-compose up
+    ```
+
+    * Acrescentar ao ficheiro *docker-compose.yml* :
+
+        ```bash
+        networks:
+          default:
+            external:
+              name: docker-hadoop_default
+        ```
+
+* *Dockerfile* :
+
+    ```dockerfile
+    FROM bde2020/spark-base
+    COPY target/jarname.jar /
+    ENTRYPOINT ["/spark/bin/spark-submit", "--class", "mainclass", "--master", "spark://spark-master:7077", "/jarname.jar"]
+    ```
+
+    * Opções de execução :
+
+        ```bash
+        -p 4040:4040 --network docker-hadoop_default --env-file ../docker-hadoop/hadoop.env
+        ```
+
+* *Clean up with* :
+
+    ```bash
+    $ docker-compose down
+    $ docker volume prune
+    ```
+
+    &rarr; Transferência do ficheiro *title.principals.tsv* para o *HDFS* :
+
+    ```bash
+    $ docker run --network docker-hadoop_default --env-file docker-hadoop/hadoop.env -it bde2020/hadoop-base bash
+    
+    $ hdfs dfs -mkdir /data
+    
+    $ curl https://datasets.imdbws.com/title.principals.tsv.gz | gunzip | hdfs dfs -put - hdfs://namenode:9000/data/title.principals.tsv
+    ```
+
 ### *Streamgen*
 
 ***Streamgen*** : gerador de dados para teste de processamento de *streams*.
--- TP2/TP2/src/main/java/batch/Top10.java
+++ TP2/TP2/src/main/java/batch/Top10.java
@@ -1,10 +1,61 @@
 package batch;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.spark_project.guava.collect.Iterators;
+import scala.Serializable;
+import scala.Tuple2;
+
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Top10
  */
 public class Top10 {
-    public static void main(String[] args) {
+    /**
+     * Tuple2's Comparator
+     */
+    public static class MyComparator implements Serializable, Comparator<Tuple2<Integer, String>> {
+        @Override
+        public int compare(Tuple2<Integer, String> t1, Tuple2<Integer, String> t2) {
+            return t1._1.compareTo(t2._1);
+        }
+    }
 
+    public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+
+        // Spark configuration
+        SparkConf conf = new SparkConf().setAppName("Top10");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        // Initial processing
+        JavaPairRDD<String, String> jprdd = sc
+            .textFile("hdfs://namenode:9000/data/title.principals.tsv.bz2")
+            .map(l -> l.split("\t"))
+            .filter(l -> !l[0].equals("tconst") && !l[2].equals("nconst"))
+            .mapToPair(l -> new Tuple2<>(l[2], l[0]));
+
+        // Top 10
+        List<Tuple2<Integer, String>> result = jprdd
+            .groupByKey()
+            .mapToPair(pair -> {
+                return new Tuple2<>(pair._1, Iterators.size(pair._2.iterator()));
+            })
+            .mapToPair(pair -> new Tuple2<>(pair._2, pair._1))
+            .top(10, new MyComparator());
+
+        // Output result
+        System.out.println("\nTop 10:\n");
+        for (Tuple2<Integer, String> t : result) {
+            System.out.println(t._2 + " : " + t._1);
+        }
+
+        // Close spark context
+        sc.close();
+
+        System.out.println("\nTime: " + (System.currentTimeMillis() - time) + " ms");
     }
 }
