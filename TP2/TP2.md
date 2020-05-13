723f4d4dd5264643a8195fdca3b635aa4f01baf7
Nelson198 <nteixeira88@gmail.com>
2020-05-13 18:58:19 +0100

    #40 Commit: Preparação do TP2

---
+++ TP2/TP2.md
@@ -0,0 +1,37 @@
+# Gestão de Grandes Conjuntos de Dados - 2019/2020
+
+## TP2
+
+### *Streamgen*
+
+***Streamgen*** : gerador de dados para teste de processamento de *streams*.
+
+* Como compilar :
+
+    ```bash
+    $ mvn package
+    $ docker build -t streamgen
+    ```
+
+* Modo de usar como servidor local :
+
+    ```bash
+    $ java -jar target/TP2-1.0-SNAPSHOT.jar ./title.ratings.tsv.gz 120
+    ```
+
+* Mode de usar como *container* *docker* ligado a uma rede *Hadoop* :
+
+    ```bash
+    $ docker run --env-file hadoop.env --network docker-hadoop_default -p 12345:12345 run streamgen hdfs:///input/title.ratings.tsv 120
+    ```
+
+* Parâmetros :
+    * **1º parâmetro**: localização do ficheiro do *IMDb* (local, *http* ou *hdfs*);
+    * **2º parâmetro:** número de eventos gerados por minuto.
+
+Para testar, ligar a `localhost:12345`, por exemplo com :
+
+```bash
+$ nc localhost 12345
+```
+
---
+++ TP2/TP2/src/main/java/streamgen/Main.java
@@ -0,0 +1,152 @@
+package streamgen;
+
+import org.apache.commons.compress.compressors.CompressorStreamFactory;
+import org.apache.commons.rng.UniformRandomProvider;
+import org.apache.commons.rng.sampling.distribution.AhrensDieterExponentialSampler;
+import org.apache.commons.rng.sampling.distribution.ContinuousSampler;
+import org.apache.commons.rng.simple.RandomSource;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Main
+ */
+public class Main {
+    private static Logger log = LoggerFactory.getLogger(Main.class);
+
+    /**
+     * Entry
+     */
+    private static class Entry implements Comparable<Entry> {
+        public String id;
+        public float rating;
+        public long votes;
+
+        /**
+         * Parameterized constructor
+         * @param id Identifier
+         * @param rating Rating
+         * @param votes Votes
+         */
+        public Entry(String id, float rating, long votes) {
+            this.id = id;
+            this.rating = rating;
+            this.votes = votes;
+        }
+
+        @Override
+        public int compareTo(Entry entry) {
+            return Long.compare(this.votes, entry.votes);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String name = args.length < 1 ? "title.ratings.tsv.bz2" : args[0];
+        double rate = args.length < 2 ? 120 : Double.parseDouble(args[1]);
+
+        log.info("reading IMDb ratings from {}, generating {} events/min", name, rate);
+
+        InputStream raw;
+        FileSystem fs = null;
+
+        if (name.startsWith("hdfs:")) {
+            Configuration conf = new Configuration();
+            conf.addResource(new Path("/etc/hadoop/core-site.xml"));
+            conf.addResource(new Path("/etc/hadoop/hdfs-site.xml"));
+            fs = FileSystem.get(conf);
+            raw = fs.open(new Path(name));
+        } else if (name.startsWith("http:") || name.startsWith("https:")) {
+            raw = new URL(name).openStream();
+        } else {
+            raw = new FileInputStream(name);
+        }
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(
+                        new CompressorStreamFactory().createCompressorInputStream(
+                                new BufferedInputStream(raw))));
+
+        List<Entry> titles = new ArrayList<>();
+
+        br.readLine(); // discard header
+        String line = br.readLine();
+        long votes = 0;
+        while (line != null) {
+            String[] row = line.split("\\t");
+            votes += Integer.parseInt(row[2]);
+            titles.add(new Entry(row[0], Float.parseFloat(row[1]), votes));
+            line = br.readLine();
+        }
+
+        br.close();
+        if (fs != null)
+            fs.close();
+
+        log.info("read {} lines", titles.size());
+
+        UniformRandomProvider rng = RandomSource.create(RandomSource.MT_64);
+        ContinuousSampler iat = new AhrensDieterExponentialSampler(rng, 1/rate);
+
+        ServerSocket ss = new ServerSocket(12345);
+        while (true) {
+            log.info("waiting for connection on {}", ss.getLocalSocketAddress());
+            try (Socket s = ss.accept()) {
+                log.info("new connection from {}", s.getRemoteSocketAddress());
+
+                // kill the connection if something is received
+                new Thread(() -> {
+                    try {
+                        s.getInputStream().read();
+                    } catch(Exception e) {
+                        // don't care
+                    }
+                    try {
+                        s.close();
+                    } catch (IOException e) {
+                        // don't care
+                    }
+                }).start();
+
+                PrintWriter pw = new PrintWriter(new OutputStreamWriter(s.getOutputStream()));
+
+                Entry target = new Entry("TARGET", 0, 0);
+                while (!s.isClosed()) {
+                    Thread.sleep((long) (iat.sample() * 1000 * 60));
+
+                    target.votes = rng.nextLong(votes);
+                    int pos = Collections.binarySearch(titles, target);
+                    if (pos < 0)
+                        pos = -pos;
+                    Entry entry = titles.get(pos);
+                    int rating = (int) (Math.floor(Math.min(10.0, rng.nextFloat() * entry.rating * 2.0)));
+                    pw.println(entry.id + "\t" + rating);
+                    pw.flush();
+                }
+            } catch (Exception e) {
+                // don't care
+            }
+            log.info("connection closed");
+        }
+    }
+}
